b4a3e1e630235ef92bc94a255a78ac15acce4640
uanoob <usnet@ukr.net>
2019-01-11 12:41:07 +0200

    Create ApolloBoost client & connect to Graphql server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log('js ok');
+import ApolloBoost, { gql } from 'apollo-boost';
+
+const client = new ApolloBoost({
+  uri: 'http://localhost:4000',
+});
+
+const getPosts = gql`
+  query {
+    posts {
+      title
+      author {
+        name
+      }
+    }
+  }
+`;
+
+client
+  .query({
+    query: getPosts,
+  })
+  .then((response) => {
+    console.log(response.data);
+    let template = '';
+
+    response.data.posts.map((post) => {
+      template += `
+        <h3>${post.title}</h3>
+       <h4>${post.author.name}</h4>
+      `;
+      return null;
+    });
+
+    document.getElementById('posts').innerHTML = template;
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
